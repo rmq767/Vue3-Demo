0d5b612626db30a35a1b70980bb6788ca831bd6d
rmq <ruanmaoqi@qq.com>
2024-05-17 15:43:28 +0800

    feat: 优化GIS代码

--- src/views/Cesium/getData.vue
+++ src/views/Cesium/getData.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="gis-getdata">
+    <div class="code">{{ state.code }}</div>
     <div class="handle">
-      <el-radio-group v-model="state.drawType">
+      <el-radio-group v-model="state.drawType" @change="changeType">
         <el-radio-button label="画点" :value="CesiumDataType.Point" />
         <el-radio-button label="画线" :value="CesiumDataType.Polyline" />
         <el-radio-button label="画面" :value="CesiumDataType.Polygon" />
@@ -18,71 +19,56 @@ export default { name: "getData" };
 </script>
 <script lang="ts" setup>
 import GisSign from "@/components/Gis/sign.vue";
-import { reactive, ref } from "vue";
-import { CesiumDataType } from "@/types/gis";
+import { reactive, ref, watch } from "vue";
+import { CesiumDataType, CesiumDataItem, CesiumDrawGetData } from "@/types/gis";
 
 const state = reactive({
   drawType: CesiumDataType.Point as CesiumDataType,
   formData: {
-    pointData: [
-      // {
-      //   longitude: 104.04410117961203,
-      //   latitude: 30.542082990470284,
-      //   name: "测试1",
-      //   id: "1-1-1",
-      //   flag: "1",
-      //   remark: "备注1",
-      // },
-      // {
-      //   longitude: 104.04403855249438,
-      //   latitude: 30.539679599708723,
-      //   name: "测试2",
-      //   id: "1-1-2",
-      //   flag: "2",
-      //   remark: "备注2",
-      // },
-      // {
-      //   longitude: 104.04614545132368,
-      //   latitude: 30.539697621757444,
-      //   name: "测试3",
-      //   id: "1-1-3",
-      //   flag: "",
-      //   remark: "备注3",
-      // },
-      // {
-      //   longitude: 104.04613294234225,
-      //   latitude: 30.542067148690677,
-      //   name: "测试4",
-      //   id: "1-1-4",
-      //   flag: "",
-      //   remark: "",
-      // },
-    ],
+    pointData: [] as CesiumDataItem[],
   },
+  code: "",
 });
+
+watch(
+  () => state.formData.pointData,
+  (newVal, oldVal) => {
+    const jsonString = JSON.stringify(newVal);
+    state.code = jsonString;
+  },
+  {
+    deep: true,
+    immediate: true,
+  }
+);
 
 const gisSignRef = ref<InstanceType<typeof GisSign>>();
 const openGisSign = () => {
-  gisSignRef.value?.open(state.drawType);
+  gisSignRef.value?.open(state.drawType, state.formData.pointData);
 };
-const savePointData = (data: any) => {
-  console.log(data);
-  // if (data) {
-  //   state.formData.pointData = data.map((item: any) => {
-  //     return {
-  //       index: item.index + 1,
-  //       longitude: item.degrees[0],
-  //       latitude: item.degrees[1],
-  //     };
-  //   });
-  // }
+const savePointData = (data: CesiumDrawGetData[]) => {
+  if (data) {
+    state.formData.pointData = data.map((item) => {
+      return {
+        lon: item.degrees[0],
+        lat: item.degrees[1],
+      };
+    });
+  }
+};
+const changeType = () => {
+  state.formData.pointData = [];
 };
 </script>
 
 <style lang="scss" scoped>
 .gis-getdata {
   width: 100%;
-  height: 100%;
+  // height: 100%;
+  .code {
+    min-height: 40vh;
+    margin-bottom: 20px;
+  }
 }
 .handle {
   display: flex;
